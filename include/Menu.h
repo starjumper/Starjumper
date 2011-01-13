8535f5c127aa7e3ede85ffb093fe5affc19a2831
Robert Strobl <robert.strobl@student.hpi.uni-potsdam.de>
2011-01-13 13:56:35 +0100

    first menu implementation by aligning labels in a box widget

--- include/Menu.h
+++ include/Menu.h
@@ -1,14 +1,119 @@
 #pragma once
 
+#include <iostream>
+#include <osgGA/StateSetManipulator>
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+#include <osgWidget/Util>
+#include <osgWidget/WindowManager>
+#include <osgWidget/Box>
+#include <osgWidget/Label>
+#include <osgWidget/ViewerEventHandlers>
+#include <osg/ShapeDrawable>
+#include <osg/Geode>
+
+
 #include "RenderingInstance.h"
 #include "types.h"
+
+const unsigned int MASK_2D = 0xF0000000;
+const unsigned int MASK_3D = 0x0F000000;
+
+struct ColorLabel: public osgWidget::Label {
+	ColorLabel(const char* label): osgWidget::Label("", "") 
+	{
+		setFont("fonts/times.ttf");
+		setFontSize(14);
+		setFontColor(0.0f, 0.0f, 0.0f, 1.0f);
+//		setColor(0.5f, 0.5f, 0.5f, 1.0f);
+//		addHeight(18.0f);
+//		setCanFill(true);
+		setLabel(label);
+		setPadding(10.0f);
+		addSize(21.0f, 22.0f);
+		setColor(1.0f, 0.5f, 0.0f, 0.0f);
+//		setEventMask(osgWidget::EVENT_MOUSE_PUSH | osgWidget::EVENT_MASK_MOUSE_MOVE);
+	}
+/*
+	bool mousePush(double, double, osgWidget::WindowManager*) 
+	{
+		return true;
+	}
+
+	bool mouseEnter(double, double, osgWidget::WindowManager*) 
+	{
+		setColor(0.6f, 0.6f, 0.6f, 1.0f);
+        return true;
+    }
+
+    bool mouseLeave(double, double, osgWidget::WindowManager*) 
+	{
+        setColor(0.3f, 0.3f, 0.3f, 1.0f);
+        return true;
+    }*/
+};
+
+class ColorLabelMenu: public ColorLabel {
+    osg::ref_ptr<osgWidget::Window> _window;
+
+public:
+    ColorLabelMenu(const char* label):
+    ColorLabel(label) {
+        _window = new osgWidget::Box(
+            std::string("Menu_") + label,
+            osgWidget::Box::VERTICAL,
+            true
+        );
+
+        _window->addWidget(new ColorLabel("Open Some Stuff"));
+        _window->addWidget(new ColorLabel("Do It Now"));
+        _window->addWidget(new ColorLabel("Hello, How Are U?"));
+        _window->addWidget(new ColorLabel("Hmmm..."));
+        _window->addWidget(new ColorLabel("Option 5"));
+
+        _window->resize();
+
+        setColor(0.8f, 0.8f, 0.8f, 0.8f);
+    }
+
+    void managed(osgWidget::WindowManager* wm) {
+        osgWidget::Label::managed(wm);
+
+        wm->addChild(_window.get());
+
+        _window->hide();
+    }
+
+    void positioned() {
+        osgWidget::Label::positioned();
+
+        _window->setOrigin(getX(), getHeight());
+        _window->resize(getWidth());
+    }
+
+    bool mousePush(double, double, osgWidget::WindowManager*) {
+        if(!_window->isVisible()) _window->show();
+
+        else _window->hide();
+
+        return true;
+    }
+
+    bool mouseLeave(double, double, osgWidget::WindowManager*) {
+        if(!_window->isVisible()) setColor(0.8f, 0.8f, 0.8f, 0.8f);
+
+        return true;
+    }
+};
 
 class Menu : public RenderingInstance
 {
 private:
     
 public:
-    Menu();
-    virtual void configureViewer(osgViewer::Viewer *viewer);
+	Menu();
+	
+	virtual void configureViewer(osgViewer::Viewer *viewer);
     virtual void handleUserInput(Key key, KeyState keyState);
 };
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -6,9 +6,65 @@ Menu::Menu() :
     
 }
 
+
+osgWidget::Label* createLabel(const std::string& l, unsigned int size=13) {
+	osgWidget::Label* label = new osgWidget::Label("", "");
+	
+	    label->setFont("fonts/times.ttf");
+    	label->setFontSize(size);
+	    label->setFontColor(1.0f, 1.0f, 1.0f, 1.0f);
+	    label->setLabel(l);
+		label->setPadding(3.0f);
+		label->setColor(0.0f, 0.0f, 1.0f, 0.5f);
+		label->setAlignHorizontal(osgWidget::Widget::HA_LEFT);
+	
+	    return label;
+}
+
 void Menu::configureViewer(osgViewer::Viewer *viewer)
 {
+    osgWidget::WindowManager* wm = new osgWidget::WindowManager(
+        viewer,
+       	viewer->getCamera()->getViewport()->width(),
+       	viewer->getCamera()->getViewport()->height(),
+        MASK_2D,
+        // osgWidget::WindowManager::WM_USE_RENDERBINS |
+        osgWidget::WindowManager::WM_PICK_DEBUG
+    );
     
+    osgWidget::Box*   vbox   = new osgWidget::Box("vbox", osgWidget::Box::VERTICAL);
+
+    // Setup the labels for the vertical box.
+    osgWidget::Label* label1 = createLabel("Start game", 60);
+    osgWidget::Label* label2 = createLabel("Settings", 60);
+    osgWidget::Label* label3 = createLabel("Highscore", 60);
+    osgWidget::Label* label4 = createLabel("Quit", 60);
+
+    vbox->addWidget(label1);
+    vbox->addWidget(label2);
+    vbox->addWidget(label3);
+    vbox->addWidget(label4);
+    vbox->attachMoveCallback();
+    vbox->attachScaleCallback();
+
+    vbox->resize();
+
+    vbox->setVisibilityMode(osgWidget::Window::VM_ENTIRE);
+//    vbox->setVisibleArea(50, 50, 500, 200);
+    vbox->setAnchorVertical(osgWidget::Window::VA_CENTER);
+    vbox->setAnchorHorizontal(osgWidget::Window::HA_CENTER);
+
+	viewer->getCamera()->setClearColor(osg::Vec4( 0., 0., 0., 1. )); 
+
+    // Test our label-in-window copy construction...
+    wm->addChild(vbox);
+
+    osg::Camera* camera = wm->createParentOrthoCamera();
+
+    getRootNode()->addChild(camera);
+    wm->resizeAllWindows();
+
+//    osgWidget::createExample(*viewer, wm);
 }
 
 void Menu::handleUserInput(Key key, KeyState keyState)
